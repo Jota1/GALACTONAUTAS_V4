16894e4415b5f2600075aa43e15941b2975d2e58
Joao Pedro Sodre <jpasodre@hotmail.com>
2020-05-25 19:07:03 -0300

    .

---
+++ Assets/AI.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AI : MonoBehaviour
+{
+    public GameObject panelAI;
+    public string[] combustivel50;
+    public string[] combustivel0;
+
+    [TextArea(3,10)]
+    public string[] narrativa;
+    public float duraçãoDasFalas;
+
+    public Text text;
+
+    private bool falaAtiva;
+    private int narrativaAtual;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        narrativaAtual = 0;
+        timer = duraçãoDasFalas;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+       
+
+        if (falaAtiva)
+        {
+            timer -= Time.timeScale;
+
+            if (!panelAI.activeSelf)
+            {
+                panelAI.SetActive(true);
+                text.text = narrativa[narrativaAtual];
+            }
+
+            if (timer <= 0)
+            {
+                panelAI.SetActive(false);
+                timer = duraçãoDasFalas;
+                narrativaAtual++;
+                falaAtiva = false;
+
+            }
+        }
+    }
+
+
+    public void SetFalaAtiva(bool state)
+    {
+        falaAtiva = state;
+    }
+}
---
+++ Assets/Dome.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dome : MonoBehaviour
+{
+    private float time;
+    private float segundos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 1;
+        segundos = time * 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        segundos -= Time.deltaTime;
+
+        if (segundos <= 0)
+        {
+            Generators.currentEnergy -= 1;
+            UpdateInterface.instance.Update2();
+            segundos = time * 60;
+        }
+    }
+}
